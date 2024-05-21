0525ddb677dc9917556f8472f5acdd0fa9504b13
Alan <alanrjes@gmail.com>
2024-05-21 00:25:35 -0700

    switched to using mask to narrow bubble detection

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 import pdfkit
 import cv2
 from pdf2image import convert_from_path
+import numpy as np
 
 # global utility function:
 def change_window(currentFrame, nextFrame):
@@ -75,30 +76,28 @@ class CreateSurvey(Frame):
         filename = self.title.get()
         if filename == "":
             filename = "Survey"
-        i = 0
-        for f in os.listdir("./print"):
-            if f == filename+".pdf" or f == filename+"("+str(i)+")"+".pdf":
-                i+=1
-        if i:
-            filename +="("+str(i)+")"
+        i = 1
+        usedNames = os.listdir("./print")
+        tempName = filename
+        while tempName+".pdf" in usedNames:
+            tempName = filename + "("+str(i)+")"
+            i+=1
+        filename = tempName
         
         # build PDF
         def print_questions():  # helper for printing out questions & bubbles
-            labels = "".join(["<td style='text-align: center'>"+str(i)+"</td>" for i in range(1,6)])
-            bubbles = "<td style='font-size: 12pt'>&#x25EF</td>"*5
-            alignmentSquares = "<tr><td></td>"+"<td style='text-align: center'>&#x25A0</td>"*5+"</tr>"
+            labels = "".join(["<td style='padding: 10px; text-align: center; border-bottom: 1px solid black'>"+str(i)+"</td>" for i in range(1,6)])
+            bubbles = "<td style='padding: 10px; font-size: 12pt'>&#x25EF</td>"*5  # for debugging, filled circle: 25CF
 
-            htmlstr = "<tr><td></td>"+labels
+            htmlstr = "<tr><td style='padding: 10px; border-right: 1px solid black; border-bottom: 1px solid black'><b>Questions</b></td>"+labels
 
             for q in self.questions:
-                htmlstr += '''<tr>
-                                <td>'''+q.get()+'''</td>
+                htmlstr += '''<tr style="padding: 10px">
+                                <td style="padding: 10px; width: 100%; border-right: 1px solid black">'''+q.get()+'''</td>
                                 '''+bubbles+'''
-                                <td>&#x25C0</td>
                             </tr>'''
             
-            htmlstr += alignmentSquares
-            return "<table style='border-spacing: 20px'>"+htmlstr+"</table>"
+            return "<table style='width: 100%; border-collapse: collapse; border: 1px solid black'>"+htmlstr+"</table>"
 
         HTMLstring = '''
             <h1>'''+self.title.get()+'''</h1>
@@ -142,34 +141,52 @@ class ScanSurvey(Frame):
         self.scanButton.grid()
     
     def scan_pages(self):
-        for f in os.listdir("./temp"):
-            data = self.scan_image(cv2.imread(f))
+        for f in os.listdir("./temp/"):
+            data = self.scan_image(f)
+            os.remove("./temp/"+f)
             print(data)
 
-    def scan_image(self, image):
-        # threshold for gray
+    def scan_image(self, f):
+        # prepare image
+        image = cv2.imread("./temp/"+f)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-        # use contours to detect alignment shapes & filled bubbles
-        tri = []
-        sqr = []
-        circ = []
+        # use contours to detect table quadrants
         contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        for contour in contours[1::]:  # ignore first contour which is full image
+        bubbleBox = contours[2]
+        mask = np.zeros(image.shape[:2], dtype="uint8")
+        cv2.rectangle(mask, cv2.boundingRect(bubbleBox), 255, -1)
+        masked = cv2.bitwise_and(image, image, mask=mask)
+#        cv2.imwrite("./temp/mask.jpg", masked)
+
+        # detect bubbles
+        gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+        _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+        bubbles, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for b in bubbles[1::]:
+            cv2.drawContours(masked, [b], 0, (0, 0, 255), 5)
+        cv2.imwrite("./temp/mask.jpg", masked)
+
+
+        a='''
             approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True) 
-            cv2.drawContours(self.image, [contour], 0, (0, 0, 255), 5) 
+            cv2.drawContours(image, [contour], 0, (0, 0, 255), 5) 
             # find center of shape
             M = cv2.moments(contour)
             if M['m00'] != 0.0:
                 x = int(M['m10']/M['m00'])
                 y = int(M['m01']/M['m00'])
-            if len(approx) == 3:    # alignment triangles (along right edge)
+            cv2.putText(image, str(len(approx)), (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
+            if np.floor(len(approx)/2) == 3:    # alignment triangles (along right edge)
                 tri.append((x, y))
-            if len(approx) == 4:    # alignment squares (along bottom edge)
-                sqr.append((x, y))
-            else:                   # circles (filled bubbles)
+            if np.floor(len(approx)/2) == 4:    # alignment squares (along bottom edge)
+                bounds = cv2.boundingRect(contour)
+            elif len(approx) == 11:  # filled circles
                 circ.append((x, y))
+            elif len(approx) != 12:  # not unfilled circles
+#                raise Exception("Unknown Symbol")
+                pass
 
         assert len(sqr) == 5
 
@@ -178,6 +195,10 @@ class ScanSurvey(Frame):
         rows = [tri[i][1]+(tri[i+1][1]-tri[i][1])/2 for i in range(len(tri)-2)]
         sqr = sorted(sqr, key=lambda c: c[0])
         cols = [sqr[i][0]+(sqr[i+1][0]-sqr[i][0])/2 for i in range(3)]
+
+        # confirm that alignment shapes are roughly straight
+        assert abs(sum([tri[i][0]-tri[i+1][0] for i in range(len(tri)-2)])) < 100
+        assert abs(sum([sqr[i][1]-sqr[i+1][1] for i in range(3)])) < 100
 
         # transfer filled bubbles to array
         data = [[0,0,0,0,0]*len(tri)]
@@ -196,7 +217,7 @@ class ScanSurvey(Frame):
             else:
                 data[q][4] = 1
 
-        return data
+        return data'''
 
 class ManageTemplates(Frame):
     def __init__(self, root):
